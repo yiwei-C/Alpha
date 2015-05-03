0e0783a92f91befd1dc6d4fa020aec8078c1b7b4
ywchenbu <Yiwei@wireless1x-168-122-1-176.bu.edu>
2015-05-03 14:04:52 -0400

    v1

---
+++ cloud.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+containerName="myapp"
+hostIP=1234
+ImageName="webapp"
+
+ssh -t 127.0.0.1 "sudo command"
+
+sudo docker -d &
+sleep 5
+
+echo "Killing previous docker container"
+
+sudo docker kill $containerName
+sudo docker rm $containerName
+
+################OPTIONAL COMMAND################
+#if [ -f /var/run/docker.pid ]; then
+#sudo rm /var/run/docker.pid
+#fi
+################OPTIONAL COMMAND################
+
+echo "Starting new docker container"
+cd ..
+sudo docker build -t=$ImageName ninja_publish
+sudo docker run -d --name $containerName -p=\"$hostIP:80\" $ImageName 
